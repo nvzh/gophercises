841619f7a2e782c9d9beedb47a31b0d30b5ec92e
Oleksander Novozhylov <onovozhylov@vidscale.com>
2018-11-22 13:31:19 +0200

    Added 'exit' function

--- quiz/main.go
+++ quiz/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-
+	// Define variable for csv file specifying
 	csvFile := flag.String("csv", "problems.csv", "Specify a csv file with questions")
 	flag.Parse()
-	_ = csvFile
+	//_ = csvFile
+
+	// The file parsing
+	file, err := os.Open(*csvFile)
+	if err != nil {
+		exit(fmt.Sprintf("Unble to open the file. Check the path or so. There is no any \"%s\"\n", *csvFile))
+	}
+	_ = file
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
